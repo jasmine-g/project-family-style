4f31457232da8738f32c46660840cd62c48e5329
jasmine-g <s10223451@connect.np.edu.sg>
2021-12-01 18:30:52 +0800

    added meta tag and media query for mobile phones

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var x = 0
+
+ function show(){
+    if(x%2 == 0){
+        document.getElementById('sneakpeak')
+        .style.display = "flex";
+
+          
+      } else {
+    document.getElementById('sneakpeak')
+    .style.display = "none"; }
+
+    x++;
+};
+    
+
+
+
+ 
